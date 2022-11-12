3b6d0f4eef83dc2ea3d480644f0d1a0eca33e643
George Treneff <treneff@protonmail.com>
2022-11-12 20:51:31 +0000

    tasks new,add,edit,delete functions completed

--- controllers/task_controller.py
+++ controllers/task_controller.py
@@ -3,6 +3,7 @@ from flask import Blueprint, Flask, redirect, render_template, request
 import repositories.task_repository as task_repository
 import repositories.user_repository as user_repository
 import repositories.project_repository as project_repository
+from models.task import Task
 
 tasks_blueprint = Blueprint("tasks", __name__)
 
@@ -11,6 +12,33 @@ tasks_blueprint = Blueprint("tasks", __name__)
 def tasks():
     tasks = task_repository.select_all()
     return render_template("tasks/index_tasks.html", tasks = tasks)
+
+#ROUTE NEW TASK --- NEEDS REFORMATTING
+@tasks_blueprint.route("/tasks/new")
+def new_task():
+    users = user_repository.select_all()
+    projects = project_repository.select_all()
+    return render_template("tasks/new.html", users=users, projects=projects)
+
+#ROUTE CREATE TASK
+@tasks_blueprint.route("/tasks", methods=["POST"])
+def create_task():
+    user_id = request.form["user_id"]
+    project_id = request.form["project_id"]
+    title = request.form["title"]
+    description = request.form["description"]
+    user = user_repository.select(user_id)
+    project = project_repository.select(project_id)
+    new_task = Task(title, description, user, project)
+    task_repository.save(new_task)
+    return redirect("/tasks")
+
+#ROUTE SHOW TASK
+@tasks_blueprint.route("/tasks/<id>", methods=['GET'])
+def show_task(id):
+    task = task_repository.select(id)
+    return render_template('tasks/show.html', task = task)
+
 #ROUTE EDIT TASK
 @tasks_blueprint.route("/tasks/<id>/edit")
 def edit_task(id):
