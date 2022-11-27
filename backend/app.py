44b34cc79bb5f50420e35a790e010ffd0628f754
George Treneff <treneff@protonmail.com>
2022-11-27 23:18:12 +0000

    flask-qslalchemy setup(unable to connect to DB via insomnia- TO FIX

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,21 @@
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
 from controllers.task_controller import tasks_blueprint
 from controllers.project_controller import projects_blueprint
+
 app = Flask(__name__)
+
+db = SQLAlchemy()
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://trenello_task_manager.db"
+db.init_app(app)
 
 app.register_blueprint(tasks_blueprint)
 app.register_blueprint(projects_blueprint)
 
 @app.route('/')
 def home():
-    return render_template("/index.html")
+    return "hey"
 
 if __name__ == '__main__':
     app.run(debug=True)
