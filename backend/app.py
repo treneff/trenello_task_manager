06791c936de1eaa3760cfec0f12b70ba033be47a
George Treneff <treneff@protonmail.com>
2022-11-29 19:20:23 +0000

    bugfixes after splitting the project between frontend and backend

--- backend/app.py
+++ backend/app.py
@@ -1,21 +1,19 @@
 from flask import Flask, render_template
-from flask_sqlalchemy import SQLAlchemy
-
 from controllers.task_controller import tasks_blueprint
+# from controllers.user_controller import users_blueprint
 from controllers.project_controller import projects_blueprint
-
 app = Flask(__name__)
 
-db = SQLAlchemy()
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://trenello_task_manager.db"
-db.init_app(app)
-
 app.register_blueprint(tasks_blueprint)
+# app.register_blueprint(users_blueprint)
 app.register_blueprint(projects_blueprint)
 
-@app.route('/')
+@app.route('/projects')
 def home():
-    return "hey"
+    return render_template('index.html')
+
+
+
 
 if __name__ == '__main__':
     app.run(debug=True)
--- backend/controllers/project_controller.py
+++ backend/controllers/project_controller.py
@@ -7,8 +7,8 @@ from models.task import Task
 from models.project import Project
 from models.task import Task
 
-projects_blueprint = Blueprint("projects", __name__)
 
+projects_blueprint = Blueprint("projects", __name__)
 #ROUTE ALL TASKS
 @projects_blueprint.route("/projects")
 def all_projects():
@@ -76,13 +76,12 @@ def create_task_in_project(id):
 def update_task_in_project(project_id,id):
     title = request.form['title']
     description = request.form['description']
-    user = request.form['user_id']
-    project  = request.form['project_id']
+    user = user_repository.select(request.form['user_id'])
+    project  = project_repository.select(request.form['project_id'])
     task = Task(title, description, user, project, False, id )
     task_repository.update(task)
+    project = project_repository.select(project_id)
     return redirect(f"/projects/{project_id}/tasks", project = project)
-
-
 
 #ROUTE EDIT TASK INSIDE PROJECT
 @projects_blueprint.route("/projects/<project_id>/tasks/<id>/edit")
@@ -91,6 +90,7 @@ def edit_task_in_project(project_id,id):
     project = project_repository.select(project_id)
     users = user_repository.select_all()
     projects = project_repository.select_all()
+    task_repository.delete(id)
     return render_template('tasks/edit.html', task = task, projects = projects, users=users, project = project)
 
 #ROUTE STATUS UPDATE INSIDE PROJECT
